51e93c0c7b16f245ad9bcc60f99a60eecfbaa796
ankushrawat123 <101786814+ankushrawat123@users.noreply.github.com>
2022-06-02 21:17:15 +0530

    Add files via upload

---
+++ CreateTableSQLQuery_UC2.sql
@@ -0,0 +1,12 @@
+﻿create database payroll_service_Assignment;
+select Name from sys.Databases;
+select db_name();
+use payroll_service_Assignment;
+create table employee_payroll
+(
+id int identity(1,1) primary key,
+name varchar(40) not null,
+salary money not null,
+start_date date not null
+);
+select * from employee_payroll;
