fb4b364010c3ff2e0b77b4e7995149cdc456d03f
ankushrawat123 <101786814+ankushrawat123@users.noreply.github.com>
2022-06-03 00:56:51 +0530

    Add files via upload

---
+++ AlterUpdateGenderSQLQuery_UC6.sql
@@ -0,0 +1,24 @@
+﻿create database payroll_service_Assignment;  
+select Name from sys.Databases;
+select db_name();
+use payroll_service_Assignment;
+create table employee_payroll
+(
+id int identity(1,1) primary key,
+name varchar(40) not null,
+salary money not null,
+start_date date not null
+);
+select * from employee_payroll;
+insert into employee_payroll values('Terisa',3000000,'2019-11-13');
+select * from employee_payroll;
+insert into employee_payroll values('Charlie',3000000,'2020-05-21');
+insert into employee_payroll values('Bili',1000000,'2018-01-03');
+insert into employee_payroll values('Mark',1000000,'2018-01-03');
+Select * from employee_payroll;
+Select * from employee_payroll where name = 'bili';
+select * from employee_payroll where start_date between '2018-01-05' and '2021-02-03';
+alter table employee_payroll add gender char(1) ;
+update employee_payroll set gender = 'F' where id in (1,3);
+update employee_payroll set gender = 'M' where id in (2,4);
+select * from employee_payroll;
